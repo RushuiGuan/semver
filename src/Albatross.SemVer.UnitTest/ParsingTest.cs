977b2be4c2572032450bdcbbfd8e2f58787895b4
unknown <Rushui>
2018-09-13 01:02:01 -0400

    initial checkin

---
+++ src/Albatross.SemVer.UnitTest/ParsingTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albatross.SemVer.UnitTest
+{
+	[TestFixture]
+    public class ParsingTest
+    {
+		[TestCase("1.2.3", ExpectedResult ="1.2.3")]
+		[TestCase("1.2.3-beta", ExpectedResult = "1.2.3-beta")]
+		[TestCase("1.2.3-beta.0", ExpectedResult = "1.2.3-beta")]
+		[TestCase("1.2.3-beta.2", ExpectedResult = "1.2.3-beta.2")]
+		[TestCase("1.2.3-rc.999", ExpectedResult = "1.2.3-rc.999")]
+		public string SupportedCase(string input) {
+			var semver = new SematicVersion(input);
+			return semver.ToString();
+		}
+
+		[TestCase("a.b.c")]
+		[TestCase("1.0.c")]
+		[TestCase("1.a.c")]
+		[TestCase("a.a.1")]
+		[TestCase("")]
+		[TestCase("1")]
+		[TestCase("1.2")]
+		[TestCase("1.2-a")]
+		public void InvalidCases(string input) {
+			TestDelegate testDelegate = new TestDelegate(() => {
+				var semver = new SematicVersion(input);
+			});
+			Assert.Catch(testDelegate);
+		}
+
+		[TestCase("1.2.3-a.b")]
+		[TestCase("1.2.3-a.1.2")]
+		[TestCase("1.2.3-1.1.2")]
+		[TestCase("1.2.3-2-abc")]
+		[TestCase("1.2.3-2")]
+		public void ValidButNotSupported(string input) {
+			TestDelegate testDelegate = new TestDelegate(() => {
+				var semver = new SematicVersion(input);
+			});
+			Assert.Catch(testDelegate);
+		}
+	}
+}
---
+++ src/Albatross.SemVer/SematicVersion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Albatross.SemVer {
+	/// <summary>
+	/// Simplified sematic version 2.0
+	/// Only allows a label and a revision number for pre-releases
+	/// </summary>
+	public class SematicVersion {
+		const string DefaultLabel = "alpha";
+		const string Pattern = @"^(\d+)\.(\d+)\.(\d+)(-([a-zA-Z_]+)(\.(\d+))?)?$";
+		static readonly Regex regex = new Regex(Pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+
+		public int Major { get; set; }
+		public int Minor { get; set; }
+		public int Patch { get; set; }
+		public string Label { get; set; }
+		public int Revision { get; set; }
+
+		public SematicVersion(string version) {
+			Parse(version);
+
+		}
+		public SematicVersion() { }
+
+		public override string ToString() {
+			if (IsRelease) {
+				return $"{Major}.{Minor}.{Patch}";
+			} else if(Revision == 0) {
+				return $"{Major}.{Minor}.{Patch}-{Label}";
+			} else {
+				return $"{Major}.{Minor}.{Patch}-{Label}.{Revision}";
+			}
+		}
+
+		public bool IsRelease { get { return string.IsNullOrEmpty(Label); } }
+
+		public void BumpMajor(string label = null) {
+			Major++;
+			Minor = 0;
+			Patch = 0;
+			Revision = 0;
+		}
+
+		public void Parse(string version) {
+			Match match = regex.Match(version);
+			if (match.Success) {
+				Major =int.Parse(match.Groups[1].Value);
+				Minor = int.Parse(match.Groups[2].Value);
+				Patch = int.Parse(match.Groups[3].Value);
+				if (match.Groups[5].Success) {
+					Label = match.Groups[5].Value;
+				}
+				if (match.Groups[7].Success) {
+					Revision = int.Parse(match.Groups[7].Value);
+				}
+			} else {
+				throw new Exception($"Invalid or NotSupported Version Format: {version}");
+			}
+		}
+	}
+}
